20ab46293a51482be73b5124ef8b14a3b609ce51
Pavlo Ostrozhynskyi <ikolochko22@gmail.com>
2022-02-10 14:33:33 +0200

    add todo app

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Todo } from 'src/app/interfaces/todo';
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
+  todos:Array<Todo> = [];
+  inputTodo:string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+      { content: "First task sadsdasd asdasdas ", completed: true },
+      { content: "Second task", completed: false }
+    ]
   }
 
+  toggleDone(id: number) {
+    this.todos.map((value, i) => {
+      if (i == id) value.completed = !value.completed;
+      return value;
+    })
+  }
+
+  deleteTodo(id:number) {
+    this.todos = this.todos.filter((value, i) => i !== id)
+  }
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = "";
+  }
 }
---
+++ src/app/interfaces/todo.ts
@@ -0,0 +1,4 @@
+export interface Todo {
+    content: string,
+    completed: boolean
+}
