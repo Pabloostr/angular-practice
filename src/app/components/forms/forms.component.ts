539cc3ab50deefce235829e44a15226b14ddf5b4
Pavlo Ostrozhynskyi <ikolochko22@gmail.com>
2022-02-17 12:12:22 +0200

    corected form

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -10,41 +10,66 @@ import { Validators } from '@angular/forms';
 export class FormsComponent implements OnInit {
 
   profileForm: FormGroup;
-  formList: FormArray;
+  // formList: FormArray;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.formList = this.formBuilder.array([
-      this.profileForm = this.formBuilder.group({
-        firstName: ['', Validators.required],
-        lastName: [''],
-        email: ['', Validators.required],
-        phoneNumber: [''],
-        position: [''],
-        company: ['']
-      })
-    ])
+    // this.formList = this.formBuilder.array([
+    //   this.profileForm = this.formBuilder.group({
+    //     firstName: ['', Validators.required],
+    //     lastName: [''],
+    //     email: ['', Validators.required],
+    //     phoneNumber: [''],
+    //     position: [''],
+    //     company: ['']
+    //   })
+    // ])
+    this.profileForm = this.formBuilder.group({
+      formList: this.formBuilder.array([])
+    })
   }
 
-  addRow() {
-    this.formList.push(this.formBuilder.group({
+  getCharValuesFormControls(): FormArray {
+    return <FormArray>this.profileForm.get('formList');
+  }
+
+  buildInfoSection() {
+    return {
       firstName: ['', Validators.required],
       lastName: [''],
       email: ['', Validators.required],
       phoneNumber: [''],
       position: [''],
       company: ['']
-    })) 
+    }
+  }
+
+  addRow() {
+    // this.formList.push(this.formBuilder.group({
+    //   firstName: ['', Validators.required],
+    //   lastName: [''],
+    //   email: ['', Validators.required],
+    //   phoneNumber: [''],
+    //   position: [''],
+    //   company: ['']
+    // })) 
+    this.getCharValuesFormControls().push(
+      this.formBuilder.group(this.buildInfoSection())
+    )
   }
 
   check() {
-    console.log(this.formList)
+    console.log(this.profileForm)
   }
 
   onSubmit() {
     console.warn(this.profileForm.value);
     this.profileForm.reset();
+  }
+  
+  removeValue(index: number) {
+    this.getCharValuesFormControls().removeAt(index);
   }
 
 }
